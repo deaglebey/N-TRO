f84c2c80bb3ae6c2c6832a891b9abcfcc4a0a8c7
deaglebey <33934339+deaglebey@users.noreply.github.com>
2018-10-11 18:38:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 // -->COMMANDS<--
 
 bot.on('guildMemberAdd',member =>{
-    member.send('porno discordum n\alt satırım invite linkini go buraya');
+    member.send('Merhaba Ben Tuğçe Benim Sunucuma bekliyorum n\https://discord.gg/w6TseEu');
 
     //member.guild.defaultChannel.sendmessage(`Welcome "${member.user.username}"! Be sure to set your platform by typing "!role"`);
 
